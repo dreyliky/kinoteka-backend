7bc096eaefe8a5cbeb30a3b05eb6d8bbf241e3ea
dreyliky <dreyliky@gmail.com>
2022-06-07 00:20:54 +0300

    update

--- src/helpers/film/film-iframe-html-to-film-file-url.helper.ts
+++ src/helpers/film/film-iframe-html-to-film-file-url.helper.ts
@@ -9,7 +9,7 @@ interface TranslationsWithRawUrls {
 }
 
 export function convertFilmIframeHtmlToFileUrl(html: string, media: FilmMediaFileMetadata): string {
-    const interestedFormat = `[360p]`;
+    const interestedFormat = `[${media.maxQuality}p]`;
     const htmlPartFromIndex = (html.indexOf(htmlPartFrom) + htmlPartFrom.length);
     const htmlPartToIndex = html.indexOf(htmlPartTo, htmlPartFromIndex);
     const interestedJson = html.slice(htmlPartFromIndex, htmlPartToIndex)
