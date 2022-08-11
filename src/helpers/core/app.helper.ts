b28025d397dd5452b8ca2bffd96ff4ddc965f73c
dreyliky <dreyliky@gmail.com>
2022-08-11 17:04:44 +0300

    update

--- src/helpers/core/app.helper.ts
+++ src/helpers/core/app.helper.ts
@@ -2,17 +2,22 @@ import { networkInterfaces } from 'os';
 
 export abstract class AppHelper {
     public static readonly protocol = 'http';
-    public static ip: string;
+
+    public static get ip(): string {
+        if (!this._ip) {
+            this._ip = this.getLocalIp();
+        }
+
+        return this._ip;
+    }
 
     public static get host(): string {
         return `${this.protocol}://${this.ip}:${global.__appPort}`
     }
 
-    constructor() {
-        AppHelper.ip = this.getLocalIp();
-    }
+    private static _ip: string;
 
-    private getLocalIp(): string {
+    private static getLocalIp(): string {
         for (let newtworkInterfaces of Object.values(networkInterfaces())) {
             for (let network of newtworkInterfaces) {
                 if (
